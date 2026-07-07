a7c39060e1c03d5cfd46256f808efe0c42cb3564
fernandalillobe <fernanda.lillobece@gmail.com>
2026-07-07 17:49:20 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -44,38 +44,3 @@ observer.observe(video);
 
     setInterval(() => ir(indice + 1), 10000); // quita esta línea si no quieres autoplay
   });
-document.querySelectorAll('[data-carrusel-videos]').forEach(carrusel => {
-  const track = carrusel.querySelector('.carrusel-track');
-  const slides = carrusel.querySelectorAll('.video-slide');
-  const btnPrev = carrusel.querySelector('.prev');
-  const btnNext = carrusel.querySelector('.next');
-  let indice = 0;
-
-  // Cargar miniatura de YouTube como fondo
-  slides.forEach(slide => {
-    const videoId = slide.dataset.videoId;
-    slide.style.backgroundImage = `url(https://img.youtube.com/vi/${videoId}/hqdefault.jpg)`;
-
-    slide.addEventListener('click', () => {
-      slide.classList.add('reproduciendo');
-      slide.innerHTML = `<iframe 
-          src="https://www.youtube.com/embed/${videoId}?autoplay=1" 
-          allow="autoplay; encrypted-media" 
-          allowfullscreen></iframe>`;
-    });
-  });
-
-  function actualizar() {
-    track.style.transform = `translateX(-${indice * 100}%)`;
-  }
-
-  btnNext.addEventListener('click', () => {
-    indice = (indice + 1) % slides.length;
-    actualizar();
-  });
-
-  btnPrev.addEventListener('click', () => {
-    indice = (indice - 1 + slides.length) % slides.length;
-    actualizar();
-  });
-});
